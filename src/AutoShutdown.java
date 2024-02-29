576e3d388e72952c8a8ec0235746ded595e60099
Orinfini <Lucotis81@gmail.com>
2024-02-29 00:08:45 -0300

    Versão0.2.5

--- src/AutoShutdown.java
+++ src/AutoShutdown.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import java.awt.*;
+
 /*
     Criando uma classe abstracta ajuda a configurar o modelo que as gui's seguirá, por exemplo em cada uma das gui's elas
   terão os mesmos tamanhos e precisarão invocar seus proprios addGUicomponents() que serão exclusivos para cada subclasse
@@ -9,10 +11,11 @@ public abstract class AutoShutdown extends JFrame {
 
     }
     private void initialize (String title){
+
     // instancia as propriedades do iframe e adiciona um titulo na barra
         setTitle(title);
     // define o tamanho (em pixels)
-        setSize(420, 420);
+        setSize(400, 300);
     // encerra o programa quando a GUI é fechada
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     // define o layout para nulo para ter um layout absoluto que nos permite especificar manualmente o tamanho e a posição dos componentes
@@ -21,6 +24,7 @@ public abstract class AutoShutdown extends JFrame {
         setResizable(false);
     // inicializa a interface no centro da tela
         setLocationRelativeTo(null);
+
     // chama a subclasse  addGuiComponent();
         addGuiComponents();
     }
